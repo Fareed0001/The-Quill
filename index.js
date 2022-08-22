5b3dce5dd3436b83921ac1a0de4c890139105f5c
Farid Ibrahim <fareedibrahim0003@gmail.com>
2022-08-22 15:27:31 +0100

    First Upload

---
+++ index.js
@@ -0,0 +1,48 @@
+if (window.matchMedia("(max - width: 768 px)").matches) {
+  // NAVBAR
+  $(".nav-img-column").removeClass("col-1").addClass("col-2");
+  $(".nav-link-col").removeClass("col-11").addClass("col-10");
+  // NEWS
+  $(".btn.breaking-caret").removeClass("btn-lg");
+  // HEADLINE TEXT
+  var text = $(".carousel-text").text();
+  var len = text.length;
+
+  if (len > 40) {
+    $(".carousel-text").text($(".carousel-text").text().substr(0, 40) + " ....");
+  }
+  // CARDS
+  $(".col-auto.d-lg-block").removeClass("d-none");
+  // FOOTER
+  $(".footer-container").removeClass("container");
+}
+
+// PHONE
+if (window.matchMedia("(max-width: 479px)").matches) {
+  $(".phone-nav-button").on("click", function() {
+    $(".nav-links-div").css({"width": "70%"});
+    $(".trending-bottom-text").css({"visibility": "collapse"});
+    $("a.phone-nav-link").css({"width": "80%"});
+    $(".World-and-sports").addClass("no-hover");
+  });
+  $(".inside-nav-button").on("click", function() {
+    $(".nav-links-div, .phone-nav-link").css({"width": "0"});
+    $(".trending-bottom-text").css({"visibility": "visible"});
+  });
+  // HEADLINE TEXT
+  var text = $(".carousel-text").text();
+  var len = text.length;
+
+  if (len > 40) {
+    $(".carousel-text").text($(".carousel-text").text().substr(0, 40) + " ....");
+  }
+  // CARD
+  var text = $(".card-text").text();
+  var len = text.length;
+
+  if (len > 40) {
+    $(".card-text").text($(".card-text").text().substr(0, 40) + " ....");
+  }
+  // FOOTER
+  $(".footer-container").addClass("container");
+}
